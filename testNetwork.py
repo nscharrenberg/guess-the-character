1c19bfda58b69456782fa1a8ca5cf8c3b88250d0
Noah Scharrenberg <nscharrenberg@hotmail.com>
2019-11-03 14:43:57 +0100

    add a test script

---
+++ testNetwork.py
@@ -0,0 +1,38 @@
+import keras
+import tensorflow as tensor
+import numpy as nmp
+import matplotlib.pyplot as plt
+
+dataset = tensor.keras.datasets.mnist # Dataset of Handwritings
+(train_x, train_y), (test_x, test_y) = dataset.load_data()
+train_x = tensor.keras.utils.normalize(train_x, axis = 1)
+test_x = tensor.keras.utils.normalize(test_x, axis = 1)
+
+for train in range(len(train_x)):
+    for row in range(28):
+        for col in range(28):
+            if train_x[train][row][col] != 0:
+                train_x[train][row][col] = 1
+
+model = tensor.keras.models.load_model('number.model')
+predictions = model.predict(test_x[:200])
+
+count = 0
+
+for number in range(len(predictions)):
+    actual = test_y[number]
+    guess = (nmp.argmax(predictions[number]))
+
+    print("The number you wrote is: ", actual, " but the Neural Network guessed: ", guess)
+
+    if(guess != actual):
+        count += 1
+
+    plt.imshow(test_x[number], cmap = plt.cm.binary)
+    plt.show()
+
+print(count, " wrong guesses out of ", len(test_x))
+percentage = 100 - ((count / len(test_x)) * 100)
+print(str(percentage), "% of the guesses was correct")
+
+
